c2c591d995d5c220613d038b552ff026b4496055
Calendau GUQUET <cguquet@synexie.fr>
2024-12-02 14:47:13 +0100

    01.2

--- aoc2024/src/main/java/kall/aoc2024/Main.java
+++ aoc2024/src/main/java/kall/aoc2024/Main.java
@@ -1,10 +1,11 @@
 package kall.aoc2024;
 
 import kall.aoc2024.day01.Day01First;
+import kall.aoc2024.day01.Day01Second;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(
-            new Day01First().run());
+        //System.out.println(new Day01First().run());
+        System.out.println(new Day01Second().run());
     }
 }
---
+++ aoc2024/src/main/java/kall/aoc2024/day01/Day01Second.java
@@ -0,0 +1,42 @@
+package kall.aoc2024.day01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import kall.Shared.BaseExercise;
+
+public class Day01Second extends BaseExercise {
+    public Day01Second() throws Exception {
+        super("D:\\Dev\\AoC2024\\aoc2024\\data\\01_2.txt");
+    }
+
+    public String run() throws Exception {
+
+        List<Integer> leftList = new ArrayList<Integer>();
+        List<Integer> rightList = new ArrayList<Integer>();
+
+        for (String line : getLines()) {
+            String[] segments = line.split(" ");
+
+            var left = Integer.parseInt(Arrays.stream(segments).findFirst().get());
+            var right = Integer.parseInt(Arrays.stream(segments).reduce((x, y) -> y).get());
+
+            leftList.add(left);
+            rightList.add(right);
+        }
+
+        leftList.sort(null);
+        rightList.sort(null);
+
+        Integer totalSimilarity = 0;
+
+            for(int i = 0 ; i < leftList.size() ; i++) {
+            int similarity = leftList.get(i) * Collections.frequency(rightList, leftList.get(i));
+
+            totalSimilarity += similarity;
+        }
+
+        return totalSimilarity.toString();
+    }
+}
